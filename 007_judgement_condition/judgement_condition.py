de5d4e553c5b83bef5aba4d4d21d2416ab2112af
9eor9e <boneizo@gmail.com>
2020-03-16 20:19:07 +0800

    input again

--- 007_judgement_condition/judgement_condition.py
+++ 007_judgement_condition/judgement_condition.py
@@ -47,3 +47,33 @@ else:
 #    print('True')
 # 只要x是非零数值、非空字符串、非空list等，就判断为True，否则为False
 
+# input()函数返回的数据是str类型，str不能直接和int做对比较，如需做比较，需要先把srt转成int
+s = input('birth:')
+birth = int(s)
+if birth <= 2000:
+    print('00前')
+else:
+    print('00后')
+
+# int()函数如果接收到的是字符串，而不是合法的数字时，就会报错，程序就会退出
+# 小明身高1.75，体重80.5kg。请根据BMI公式（体重除以身高的平方）帮小明计算他的BMI指数，并根据BMI指数：
+#     低于18.5：过轻
+#     18.5-25：正常
+#     25-28：过重
+#     28-32：肥胖
+#     高于32：严重肥胖
+high = input('请输入你的身高：')
+wight = input('请输入你的体重')
+h = float(high)
+w = int(wight)
+bmi = w/(h*h)
+if bmi > 32:
+    print('严重过胖')
+elif 32 <= bmi >= 28:
+    print('肥胖')
+elif 28 <= bmi >= 25:
+    print('过重')
+elif 25 <= bmi >= 18.5:
+    print('正常')
+else:
+    print('过轻')
