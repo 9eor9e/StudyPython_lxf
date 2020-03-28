add4a18d507fa778901b36e2315e0f02f6a373cb
9eor9e <boneizo@gmail.com>
2020-03-28 19:30:10 +0800

    参考了别人的留言，完成了最后的练习。 组合参数的使用方法 掌握的程度，皮毛

--- 010_function/function_parameter.py
+++ 010_function/function_parameter.py
@@ -249,8 +249,26 @@ def product(x,y):
 print(product(1,2))
 
 # 修改
-def product(x,y,*args):
-    for n in product:
-        sj = sj * n
-    return sj
+def product(*args):
+    j = 1
+    i = 0
+    if len(args) == 0:
+        raise TypeError
+    while i < len(args):
+        j = j * args[i]
+        i += 1
+    return j
 print(product(1,2,3))
+
+### 小结
+# python的函数具有非常灵活的参数形态，既可以实现简单的调用，又可以传入非常复杂的参数。
+# 默认参数一定要用不可变对象，如果是可变对象，程序运行时会有逻辑错误！
+# 要注意定义可变参数和关键字参数的语法：
+### *args 是可变参数， args 接收的是一个 tuple
+### **kw 是关键字参数， kw 接收的是一个dict
+# 以及调用函数时如何传入可变参数和关键字参数的语法：
+### 可变参数既可以直接传入： func(1,2,3) , 又可以先组装 list 或 tuple ，再通过 *args 传入： func(*(1,2,3))
+### 关键字参数既可以直接传入： func(a=1,b=2) ,又可以先组装 dict ，再通过 **kw 传入： func(**{'a':1,'b':2})
+### 使用 *args 和 **kw 是python的习惯写法，当然也可以用其他参数名，但最好使用习惯用法。
+# 命名的关键字参数是为了限制调用者可以传入的参数名，同时可以提供默认值。
+# 定义命名的关键字参数在没有可变参数的情况下不要忘了写分隔符 * ，否则定义的将是位置参数。
